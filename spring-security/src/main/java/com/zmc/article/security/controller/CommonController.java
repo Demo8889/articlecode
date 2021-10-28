6a59eb14f2fd8030fa28c829336b23f201d7132f
Demo8889 <19111401160@ahnu.edu.cn>
2021-10-28 15:44:14 +0800

    s

---
+++ spring-security/src/main/java/com/zmc/article/security/controller/CommonController.java
@@ -0,0 +1,15 @@
+package com.zmc.article.security.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Demo
+ * @date: 2021-10-28 15:42
+ */
+@RestController
+@RequestMapping(value = "/common")
+public class CommonController {
+
+}
